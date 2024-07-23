f3ac48753363b421078b9c9f4a588359844b815a
Vahe Sargsyan <140258231+w33bvGL@users.noreply.github.com>
2024-07-23 16:00:57 +0400

    first commit to into

---
+++ nuxt.config.ts
@@ -0,0 +1,36 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    compatibilityDate: '2024-04-03',
+
+    devtools: { enabled: true },
+
+    plugins: ['~/plugins/nprogress.ts'],
+    modules: ["nuxt-gtag", 'vuetify-nuxt-module'],
+
+    gtag: {
+        id: 'G-XKSYJXXKPB'
+    },
+
+    vuetify: {
+        moduleOptions: {
+        },
+        vuetifyOptions: {
+            theme: {
+                defaultTheme: 'dark',
+                themes: {
+
+                }
+            }
+        }
+    },
+
+    css: [
+        '~/assets/root.scss',
+        `~/assets/variables.scss`,
+        'vuetify/lib/styles/main.sass',
+    ],
+
+    build: {
+        transpile: ['vuetify'],
+    }
+})
